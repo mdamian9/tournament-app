24e2280d7a77361a7f335e7a6d8745ebd151d850
mdamian9 <m.damian9@hotmail.com>
2019-07-23 23:41:05 -0700

    Installed react-router-dom and created LandingPage and UserHomePage components. Adjusted App.js and index.js to use react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import UserHomePage from './components/UserHomePage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route exact path='/' component={App} />
+        <Route exact path='/home' component={UserHomePage} />
+    </Router>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
